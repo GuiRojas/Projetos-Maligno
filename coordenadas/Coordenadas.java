a41b8672cb494836e37e224ab7f98ddd6227c2fe
franciscomaiaN <frn640@gmail.com>
2017-10-17 09:52:42 -0200

    coordenadas
    fiz ue

--- coordenadas/Coordenadas.java
+++ coordenadas/Coordenadas.java
@@ -11,4 +11,52 @@ public class Coordenadas<X> {
 		this.ordenadas = y;
 	}
 
+	public int getX(){
+		return this.absissas;
+	}
+
+	public int getY(){
+		return this.ordenadas;
+	}
+
+	public int hashCode(){
+		int ret = 44;
+
+		ret = 7*ret + new Integer(absissas).hashCode();
+		ret = 7*ret + new Integer(ordenadas).hashCode();
+
+		return ret;
+	}
+
+	public String toString(){
+		String ret = "(";
+
+		ret += new Integer (this.absissas).toString;
+		ret += "," + new Integer (this.ordenadas).toString;
+		ret += ")";
+
+		return ret;
+	}
+
+	public boolean equals(Object obj){
+		if(this==obj)
+			return true;
+
+		if(this==null)
+			return false;
+
+		if(!(obj instanceof Coordenadas))
+			return false;
+
+		Coordenadas coo = (Coordenadas)obj;
+
+		if (this.absissas != coo.absissas)
+			return false;
+
+		if(this.ordenadas != coo.ordenadas)
+			return false;
+
+		return true;
+	}
+
 }
