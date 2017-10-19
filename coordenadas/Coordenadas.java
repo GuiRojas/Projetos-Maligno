91c7eac29c76fbec260b68dd6c7015f57a2b2008
franciscomaiaN <frn640@gmail.com>
2017-10-19 09:23:44 -0200

    comentando coordenadas

--- coordenadas/Coordenadas.java
+++ coordenadas/Coordenadas.java
@@ -1,24 +1,62 @@
 package coordenadas;
 
+/**
+*	Classe para guardar coodernadas de um plano cartesiano
+*
+*	Possui construtor, get das variaveis e os metodos obrigatorios
+*
+*	@param abssissas variavel para definir o ponto no eixo X
+*	@param ordenadas variavel para definir o ponto no eixo Y
+*
+*	@since 2017
+*
+*	@author Francisco 17178 e Guilherme 17182
+*/
 public class Coordenadas<X> {
 	protected int absissas,
 				  ordenadas;
 
-
-	public Coordenadas(int x,int y){
+	/**
+*	Construtor da classe Coordenadas
+*
+*	Atribui parametros as variaveis globais
+*
+*	@param x variavel que define a absissa desejada
+*	@param y variavel que define a ordenada desejada
+*	
+*	@throw se nao passar os valores x e y	
+	*/
+	public Coordenadas(int x,int y)throws Exception{
+		if ((x == null)||(y == null))
+			throw new Exception('Parametros nao declarados');
 
 		this.absissas = x;
 		this.ordenadas = y;
 	}
 
+	/**
+*	Retorna o valor da absissa da coordenada
+*	
+*	@return retorna o valor da absissa da coodernada guardada
+	*/
 	public int getX(){
 		return this.absissas;
 	}
 
+	/**
+*	Retorna o valor da ordenada da coordenada
+*	
+*	@return retorna o valor da ordenada da coodernada guardada
+	*/
 	public int getY(){
 		return this.ordenadas;
 	}
 
+	/**
+*	Metodo obrigatorio
+*
+*	@return retorna o hash code para o usuario
+	*/
 	public int hashCode(){
 		int ret = 44;
 
@@ -28,6 +66,11 @@ public class Coordenadas<X> {
 		return ret;
 	}
 
+	/**
+*	Metodo obrigatorio
+*
+*	@return retorna uma string com as coordenadas para o usuario
+	*/
 	public String toString(){
 		String ret = "(";
 
@@ -38,6 +81,13 @@ public class Coordenadas<X> {
 		return ret;
 	}
 
+	/**
+*	Metodo obrigatorio
+*
+*	@param obj objeto que sera comparado com this
+*
+*	@return compara dois Objects e retorna se sao iguais ou nao
+	*/
 	public boolean equals(Object obj){
 		if(this==obj)
 			return true;
