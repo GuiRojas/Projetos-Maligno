1956e657cc448f79b4f9b94c430f8ac9cc45f6f2
nodoya <guiribrojas@gmail.com>
2017-10-31 10:03:30 -0200

    xd2

---
+++ expressao/fila/Fila.java
@@ -0,0 +1,248 @@
+package fila;
+
+/**
+*Fila de instancias da classe X.
+*
+*@param X Tipo de classe a ser armazenada.
+*
+*@since 2017.
+*
+*@author Francisco 17178 e Gulherme 17182.
+*/
+public class Fila<X> implements Cloneable{
+	protected Object[] vetor;
+	protected float txDC;
+
+	protected int inicio,
+				  fim,
+				  qtd;
+
+	public Fila(){
+		this(10);
+	}
+
+	/**
+	*Construtor.
+	*
+	*@param tam Tamanho do vetor.
+	*/
+	public Fila(int tam)throws Exception{
+		if(tam<=0)
+			throw new Exception("Tamanho invalido");
+
+		this.vetor = new Object[tam];
+		this.inicio = 0;
+		this.fim = -1;
+		this.qtd = 0;
+	}
+
+	/**
+	*Retorna quantidade de Instancias na Fila.
+	*
+	*@return int Quantidade de Instancias na Fila.
+	*/
+
+	public int getQtd(){
+		return this.qtd;
+	}
+
+	/**
+	*Retorna a ultima posicao preenchida da Fila.
+	*
+	*@return int Retorna a ultima posicao preenchida da Fila.
+	*/
+
+	public int getFim(){
+		return this.fim;
+	}
+
+	/**
+	*Retorna a primeira posicao preenchida da Fila.
+	*
+	*@return int Retorna a primeira posicao preenchida da Fila.
+	*/
+
+	public int getInicio(){
+		return this.inicio;
+	}
+
+	/**
+	*Adiciona uma instancia a fila. Cresce caso esteja cheia.
+	*
+	*@param x Objeto da instancia a adicionar
+	*
+	*@throw Exception Caso tente adicionar algo null 
+	*/
+
+	public void enfileire(X x)throws Exception{
+		if(x==null)
+			throw new Exception("Inserção de valor nulo");
+
+		if(this.fim+1==this.vetor.length)
+			if(this.qtd==this.vetor.length)
+				this.crescer();
+			else
+				this.fim=0;
+		else
+			this.fim++;
+
+		this.vetor[this.fim]=x;
+		this.qtd++;
+	}
+
+	protected void crescer(){
+		float mult = this.txDC/100+1;
+        int   tamNovo = (int)Math.ceil(this.vetor.length*mult);
+
+		Object[] novo = new Object[tamNovo];
+
+		for(int i=0;i<=this.vetor.length;i++)
+			novo[i]=this.vetor[i];
+
+		this.vetor = novo;
+	}
+
+	/**
+	*Retira o primeiro valor do vetor.
+	*
+	*@throw Exception Caso o vetor esteja vazio.
+	*/
+
+	public void desenfilere()throws Exception{
+		if(this.vazio())
+			throw new Exception("Não há o que ser removido");
+
+		this.vetor[this.inicio]=null;
+		if(this.inicio+1==this.vetor.length)
+			this.inicio=0;
+		else
+			this.inicio++;
+
+		this.qtd--;
+	}
+
+	/**
+	*Retorna o primeiro valor preenchido do vetor.
+	*
+	*@return Object Retorna a primeira instancia do vetor.
+	*/
+
+	public Object getElemento()throws Exception{
+		if(this.vazio())
+			throw new Exception("Não há elemento");
+
+		return this.vetor[this.inicio];
+	}
+
+	/**
+	*Verifica se o vetor esta vazio.
+	*
+	*@return boolean Verifica se o vetor esta vazio.
+	*/
+
+	public boolean vazio(){
+		if(this.qtd==0)
+			return true;
+		return false;
+	}
+
+	/**
+	*hashCode
+	*
+	*@return int hashCode
+	*/
+
+	public int hashCode(){
+		int ret = 2;
+
+		ret = ret * 13 + new Integer(this.inicio).hashCode();
+		ret = ret * 13 + new Integer(this.fim).hashCode();
+		ret = ret * 13 + new Integer(this.qtd).hashCode();
+
+		for(int i=0;i<=this.vetor.length;i++)
+			if(!(this.vetor[i]==null)){
+				ret = ret * 13 + this.vetor[i].hashCode();
+			}
+
+		return ret;
+	}
+
+	/**
+	*toString
+	*
+	*@return String toString
+	*/
+
+	public String toString(){
+		String ret = "{ ";
+
+		for(int i=0;i<=this.vetor.length;i++)
+			ret+=this.vetor[i].toString()+(i!=this.vetor.length ?",":"");
+
+		return ret + " }";
+	}
+
+	/**
+	*equals
+	*
+	*@param obj Objeto da instancia a ser comparada
+	*
+	*@return boolean Verifica se o parametro é igual ao this
+	*/
+
+	public boolean equals(Object obj){
+		if(this==obj)
+			return true;
+		if(this==null)
+			return false;
+		if(!(obj instanceof Fila))
+			return false;
+
+		Fila fil = (Fila)obj;
+
+		if(this.inicio!=fil.inicio)
+			return false;
+		if(this.fim!=fil.fim)
+			return false;
+		if(this.qtd!=fil.qtd)
+			return false;
+		for(int i=0;i<=this.vetor.length;i++){
+    			if(!(this.vetor[i].equals(fil.vetor[i])))
+				return false;
+		}
+
+		return true;
+	}
+
+	public Fila (Fila modelo) throws Exception{
+		if(modelo==null)
+			throw new Exception("Modelu nulo");
+
+		this.vetor = new Object[modelo.vetor.length];
+		for(int i=0;i<=this.vetor.length;i++){
+			if(!(modelo.vetor[i]==null)){
+                this.vetor[i] = modelo.vetor[i];
+			}
+		}
+		this.inicio=modelo.inicio;
+		this.fim=modelo.fim;
+		this.qtd=modelo.qtd;
+	}
+
+	/**
+	*Clone
+	*
+	*@return Object outra Fila igual a essa. 
+	*/
+
+	public Object clone(){
+		Fila ret=null;
+
+		try{
+			ret= new Fila(this);
+		}catch(Exception erro){}
+
+		return ret;
+	}
+
+}
---
+++ expressao/pilha/Pilha.java
@@ -0,0 +1,223 @@
+package pilha;
+
+/**
+*Classe Pilha, que armazena qualquer Classe.
+*
+*@param X Classe a ser armazenada.
+*
+*@since 2017
+*
+*@author Francisco 17178 e Guilherme 17182
+*/
+
+public class Pilha<X> implements Cloneable{
+	protected Object[] vetor;
+	protected float txDC;
+	
+	protected int topo;
+
+	public Pilha(){
+		this(10);
+	}
+
+	public Pilha(int tam){
+		this(tam,50);
+	}
+
+	/**
+	*Construtor, declara a Pilha e seu tamanho, alem da taxa de Crescimento.
+	*
+	*@param tam Tamanho da Pilha.
+	*@param tc Taxa de crescimento para quando nao houver espaço na Pilha.
+	*
+	*@throw Exception caso os valores sejam invalidos (0 ou negativo).
+	*/
+
+	public Pilha(int tam,float tc)throws Exception{
+		try{
+			if(tam<=0){
+				throw new Exception("Tamanho Inválido");
+			}
+			if(tx<=0){
+				throw new Exception("Taxa Inválida");
+			}
+
+			this.vetor = new Object[tam];
+			this.topo = -1;
+			this.txDC = tx;
+		}catch(Exception erro){}
+	}
+
+	public int getTopo(){
+		return this.topo;
+	}
+
+	/**
+	*Empilha Instancias no vetor da Pilha. Cresce o vetor caso ele esteja cheio.
+	*
+	*@param x Objeto da instancia a ser empilhada.
+	*
+	*@throw Exception caso a instancia seja null.
+	*/
+
+	public void empilhe(X x) throws Exception{
+		if (x==null) {
+			throw new Exception("Valor nulo não pode ser adicionado");
+		}
+
+		if(this.topo+1==this.vetor.length)
+			this.crescer();
+
+		this.topo++;
+
+		this.vetor[topo]=x;
+	}
+
+	/**
+	*Desempilha a instancia na maior ultima posicao preenchida do vetor.
+	*
+	*@throw Exception caso a Pilha esteja vazia.
+	*/
+
+	public void desempilhe() throws Exception{
+		if(this.vazio())
+			throw new Exception("Não há o que desempilhar!");
+
+		this.vetor[topo]=null;
+		topo--;
+	}
+
+	protected void crescer(){
+		float mult = this.txDC/100+1;
+        int tamNovo = (int)Math.ceil(this.vetor.length*mult);
+
+		Object[] novo = new Object[tamNovo];
+
+		for(int  i=0;i<=this.topo;i++)
+			novo[i]=this.vetor[i];
+
+		this.vetor = novo;
+	}
+
+	/**
+	*Retorna o valor da instancia na ultima posicao preenchida do vetor.
+	*
+	*@throw Exception caso a Pilha esteja vazia.
+	*
+	*@return Object Instancia na ultima posicao preenchida do vetor.
+	*/
+
+	public Object getElemento() throws Exception{
+		if(this.vazio())
+			throw new Exception("Pilha está vazia!");
+
+		return this.vetor[topo];
+	}
+
+	/**
+	*Verifica se a Pilha está vazia
+	*/
+
+	public boolean vazio(){
+		if(topo==-1)
+			return true;
+		return false;
+	}
+
+	/**
+	*hashCode
+	*
+	*@return int hashCode da Pilha e suas Instancia armazenadas.
+	*/
+
+	public int hashCode(){
+		int ret = 2;
+
+		ret = ret * 13 + new Integer(this.topo).hashCode();
+		ret = ret * 13 + new Float(this.txDC).hashCode();
+
+		for(int i=0;i<=this.vetor.length;i++)
+			ret = ret * 13 + this.vetor[i].hashCode();
+
+		return ret;
+	}
+
+	/**
+	*toString
+	*
+	*@return String Versão em texto de todas as instancias armazenadas.
+	*/
+
+	public String toString(){
+		String ret = "{ ";
+
+		for(int i=0;i<=this.topo;i++)
+			ret+=this.vetor[i].toString()+(i!=this.topo?",":"");
+
+		return ret + " }";
+	}
+
+	/**
+	*equals
+	*
+	*@return boolean Verifica se outra classe é identica (Em classe e conteudo) a essa.
+	*/
+
+	public boolean equals(Object obj){
+		if(this==obj)
+			return true;
+		if(this==null)
+			return false;
+		if(!(obj instanceof Pilha))
+			return false;
+
+		Pilha pil = (Pilha)obj;
+
+		if(this.topo!=pil.topo)
+			return false;
+		if(this.txDC!=pil.txDC)
+			return false;
+		for(int i=0;i<=this.vetor.length;i++){
+			if(!(this.vetor[i].equals(pil.vetor[i])))
+				return false;
+		}
+
+		return true;
+	}
+
+	/**
+	*Construtor de Copia
+	*
+	*@return Pilha outra Pilha <i>identica</i> a essa.
+	*/
+
+	public Pilha (Pilha modelo) throws Exception{
+		if(modelo==null)
+			throw new Exception("Modelu nulo");
+
+		this.vetor = new Object[modelo.vetor.length];
+		for(int i=0;i<=this.vetor.length;i++){
+
+        this.vetor[i] = modelo.vetor[i];
+		}
+		this.topo=modelo.topo;
+		this.txDC=modelo.txDC;
+	}
+
+	/**
+	*Clone
+	*
+	*@return Object Chama o construtor de copia e retorna a nova Pilha.
+	*/
+
+	public Object clone(){
+		Pilha ret=null;
+
+		try{
+			ret= new Pilha(this);
+		}catch(Exception erro){}
+
+		return ret;
+	}
+
+}
